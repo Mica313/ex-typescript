976ca5b5fedf7e794a25731b9c5ffbdb1db43c11
Micael Almeida <onixg8874@gmail.com>
2023-08-23 21:26:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function Multiplica(num1, num2) {
+    return num1 * num2;
+}
+console.log(Multiplica(20, 10));
+function DizOla(nome) {
+    return (`Olá ${nome}!`);
+}
+console.log(DizOla('Micael'));
---
+++ main.ts
@@ -0,0 +1,8 @@
+function Multiplica(num1:number, num2:number){
+    return num1 * num2;
+}
+console.log(Multiplica(20, 10));
+function DizOla(nome:string){
+    return (`Olá ${nome}!`);
+}
+console.log(DizOla('Micael'));
